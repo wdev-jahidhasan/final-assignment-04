fd1d01d3b94bb3aebc64188870a259b4ef6be41c
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-02-22 01:52:50 +0600

    total card count updated in ui using js

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+let allCards = document.querySelectorAll('.common-card').length;
+let allCardCount1 = document.getElementById('total-count-1').innerText = allCards;
+let allCardCount2 = document.getElementById('total-count-2').innerText = allCards;
