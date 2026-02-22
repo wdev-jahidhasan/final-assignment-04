e599313c40c11c62f243007becd7898a919fdb07
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-02-22 21:42:25 +0600

    rejected render pratially done

--- scripts/script.js
+++ scripts/script.js
@@ -88,10 +88,44 @@ mainContainer.addEventListener('click', function(event){
     calculateCount();
     
     renderInterview();
+    }else if(event.target.classList.contains('rejected-red')){
+        const parentNode = event.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector('.company-name').innerText;
+    const jobTitle = parentNode.querySelector('.job-title').innerText;
+    const jobType = parentNode.querySelector('.job-type').innerText;
+    const applicationStatus = parentNode.querySelector('.application-status').innerText;
+    const jobDescription = parentNode.querySelector('.job-description').innerText;
+    const interviewGreen = parentNode.querySelector('.interview-green').innerText;
+    const rejectedRed = parentNode.querySelector('.rejected-red').innerText;
+    parentNode.querySelector('.application-status').innerText = rejectedRed;
+    // event.target.classList.parentNode.parentNode.add('bg-green-500');
+    //nicher line diye application status button er color green hocche
+    // parentNode.querySelector('.application-status').classList.add('bg-green-400');
+    
+    const cardInfo ={
+        companyName,
+        jobTitle,
+        jobType,
+        applicationStatus: 'Rejected',
+        jobDescription,
+        interviewGreen,
+        rejectedRed
+    }
+
+    const ExistingCompany = rejectedList.find(item => item.companyName == cardInfo.companyName);
+    
+    
+    if(!ExistingCompany){
+       rejectedList.push(cardInfo); 
+    }
+    calculateCount();
+    
+    renderRejected();
     }
 
 })
 
+// renderInterview function
 function renderInterview(){
     filteredSection.innerHTML = '';
     for(let interview of interviewList){
@@ -118,4 +152,32 @@ function renderInterview(){
         filteredSection.appendChild(div);
     }
 }
-    
+ 
+// renderRejected function
+
+function renderRejected(){
+    filteredSection.innerHTML = '';
+    for(let rejected of rejectedList){
+        // console.log(interview)
+
+        let div = document.createElement('div');
+        div.className = 'common-card flex justify-between bg-white rounded-lg';
+        div.innerHTML = `
+            <div class="left-div p-5">
+           <h3 class="company-name text-2xl font-semibold mb-2 text-[#002C5C]">${rejected.companyName}</h3>
+           <h2 class="job-title text-xl font-medium text-gray-500 mb-4">${rejected.jobTitle}</h2>
+           <p class="job-type mb-4 text-gray-500">${rejected.jobType}</p>
+           <button class="application-status btn btn-soft mb-4 text-[#002C5C]">${rejected.applicationStatus}</button>
+           <p class="job-description mb-4">${rejected.jobDescription}</p>
+           <button class="interview-green btn btn-outline btn-success p-5">${rejected.interviewGreen}</button>
+           <button class="rejected-red btn btn-outline btn-error p-5 mx-3">${rejected.rejectedRed}</button>
+        </div>
+        <div class="right-div p-5">
+          <i class="fa-regular fa-trash-can text-gray-400 border border-gray-300 rounded-lg
+          py-1"></i>
+        </div>
+        `
+
+        filteredSection.appendChild(div);
+    }
+}
