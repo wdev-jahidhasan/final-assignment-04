0b47df9e9200dc49e03088e2f37e80898fa4c016
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-02-22 13:45:42 +0600

    button toggling okay

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,38 @@
-// total count
+// button toggling and section wise count
+
+let interviewList = [];
+let rejectedList = [];
+
 let total = document.getElementById('total-count-1');
 let total2 = document.getElementById('total-count-2');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+const allFilterBtn = document.getElementById('all-filter-button');
+const interviewFilterBtn = document.getElementById('interview-filter-button');
+const rejectedFilterBtn = document.getElementById('rejected-filter-button');
+
 const allCardsSection = document.getElementById('all-cards');
+const mainContainer = document.querySelector('main');
 
 function calculateCount(){
     total.innerText = allCardsSection.children.length;
     total2.innerText = allCardsSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
 }
 
 calculateCount();
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
+    interviewFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
+
+    allFilterBtn.classList.add('bg-gray-200', 'text-black');
+    interviewFilterBtn.classList.add('bg-gray-200', 'text-black');
+    rejectedFilterBtn.classList.add('bg-gray-200', 'text-black');
+
+    const selected = document.getElementById(id);
+    selected.classList.add('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
+}
