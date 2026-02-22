25fa55b6ab28ff0570824a247e0de21094f1ba34
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-02-22 20:25:52 +0600

    toggling between all and interview button primarily done

---
+++ README.md
@@ -0,0 +1,11 @@
+Answers to Questions
+
+Q1. What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
+
+Q2. How do you create and insert a new element into the DOM?
+
+Q3. What is Event Bubbling? And how does it work?
+
+Q4. What is Event Delegation in JavaScript? Why is it useful?
+
+Q5. What is the difference between preventDefault() and stopPropagation() methods?
--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ const rejectedFilterBtn = document.getElementById('rejected-filter-button');
 
 const allCardsSection = document.getElementById('all-cards');
 const mainContainer = document.querySelector('main');
+const filteredSection = document.getElementById('filtered-section');
 
 function calculateCount(){
     total.innerText = allCardsSection.children.length;
@@ -25,22 +26,31 @@ function calculateCount(){
 calculateCount();
 
 function toggleStyle(id){
+
     allFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
     interviewFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
     rejectedFilterBtn.classList.remove('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
 
-    allFilterBtn.classList.add('bg-gray-200', 'text-black');
-    interviewFilterBtn.classList.add('bg-gray-200', 'text-black');
-    rejectedFilterBtn.classList.add('bg-gray-200', 'text-black');
-
     const selected = document.getElementById(id);
     selected.classList.add('bg-gradient-to-r' , 'from-blue-600' , 'to-blue-800', 'text-white');
+
+    if(id == 'interview-filter-button'){
+        allCardsSection.classList.add('hidden');
+        // selected.parentNode.classList.add('bg-green-500')
+        filteredSection.classList.remove('hidden');
+    }else if (id == 'all-filter-button'){
+        allCardsSection.classList.remove('hidden');
+        filteredSection.classList.add('hidden')
+    }
+    
 }
 
 // card transferring to the category
 
 mainContainer.addEventListener('click', function(event){
-    const parentNode = event.target.parentNode.parentNode;
+    
+    if(event.target.classList.contains('interview-green')){
+        const parentNode = event.target.parentNode.parentNode;
     const companyName = parentNode.querySelector('.company-name').innerText;
     const jobTitle = parentNode.querySelector('.job-title').innerText;
     const jobType = parentNode.querySelector('.job-type').innerText;
@@ -48,6 +58,10 @@ mainContainer.addEventListener('click', function(event){
     const jobDescription = parentNode.querySelector('.job-description').innerText;
     const interviewGreen = parentNode.querySelector('.interview-green').innerText;
     const rejectedRed = parentNode.querySelector('.rejected-red').innerText;
+    parentNode.querySelector('.application-status').innerText = interviewGreen;
+    // event.target.classList.parentNode.parentNode.add('bg-green-500');
+    //nicher line diye application status button er color green hocche
+    // parentNode.querySelector('.application-status').classList.add('bg-green-400');
     
     const cardInfo ={
         companyName,
@@ -58,5 +72,43 @@ mainContainer.addEventListener('click', function(event){
         interviewGreen,
         rejectedRed
     }
+
+    const ExistingCompany = interviewList.find(item => item.companyName == cardInfo.companyName);
     
+    
+    if(!ExistingCompany){
+       interviewList.push(cardInfo); 
+    }
+    
+    renderInterview()
+    }
+
 })
+
+function renderInterview(){
+    filteredSection.innerHTML = '';
+    for(let interview of interviewList){
+        // console.log(interview)
+
+        let div = document.createElement('div');
+        div.className = 'common-card flex justify-between bg-white rounded-lg';
+        div.innerHTML = `
+            <div class="left-div p-5">
+           <h3 class="company-name text-2xl font-semibold mb-2 text-[#002C5C]">${interview.companyName}</h3>
+           <h2 class="job-title text-xl font-medium text-gray-500 mb-4">${interview.jobTitle}</h2>
+           <p class="job-type mb-4 text-gray-500">${interview.jobType}</p>
+           <button class="application-status btn btn-soft mb-4 text-[#002C5C]">${interview.applicationStatus}</button>
+           <p class="job-description mb-4">${interview.jobDescription}</p>
+           <button class="interview-green btn btn-outline btn-success p-5">${interview.interviewGreen}</button>
+           <button class="rejected-red btn btn-outline btn-error p-5 mx-3">${interview.rejectedRed}</button>
+        </div>
+        <div class="right-div p-5">
+          <i class="fa-regular fa-trash-can text-gray-400 border border-gray-300 rounded-lg
+          py-1"></i>
+        </div>
+        `
+
+        filteredSection.appendChild(div);
+    }
+}
+    
